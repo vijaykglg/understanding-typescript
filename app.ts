13ae56a84e7e569758178488c945240aebd4fe15
vijaykglg <vijay.gupta7@yahoo.in>
2021-02-10 17:12:23 +0530

    Array Types

--- app.ts
+++ app.ts
@@ -4,7 +4,15 @@
 } = { */
 const person = {
     name: "Vijay",
-    age: 37    
+    age: 37,
+    hobbies: ['Sports','Cooking']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
