d26f5b88f27a84d2096303c9b98ccaa4971433b4
vijaykglg <vijay.gupta7@yahoo.in>
2021-02-11 20:09:30 +0530

    Type Alias - Define your own type

--- app.ts
+++ app.ts
@@ -1,4 +1,21 @@
-function combine(input1: number | string,input2: number | string, resultConversion: 'as-number' | 'as-text'){
+type Combinable = number | string;//This is Type Alias
+type CombinableResult = 'as-number' | 'as-text';//This is Type Alias
+type User = { name: string; age: number };//We can define our own type and use it to declare variables
+
+function greet(user: User) { 
+     console.log('Hi, I am ' + user.name +' and my age is '+user.age);
+} 
+
+function isOlder(user: User, checkAge: number) { 
+     return checkAge > user.age;
+    
+}
+
+greet({ name: 'Vijay', age: 30 });
+const result = isOlder({ name: 'Vijay', age: 30 },25);
+console.log(result);
+
+function combine(input1: Combinable,input2: Combinable, resultConversion: CombinableResult){
     let result;
     if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
         result = +input1 + +input2;
