4d45066db508a39c03dba88a594b7afe9a63e791
vijaykglg <vijay.gupta7@yahoo.in>
2021-02-11 11:35:59 +0530

    Union Types

--- app.ts
+++ app.ts
@@ -1,29 +1,15 @@
-/* const person: {
-    name: string;
-    age: number;
-} = { */
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number,string];//To define Tuple type we need to define the type of object
-} = {
-    name: "Vijay",
-    age: 37,
-    hobbies: ['Sports','Cooking'],
-    role: [2,'Author']
+function combine(input1: number | string,input2: number | string){
+    let result;
+    if(typeof input1 === 'number' && typeof input2 === 'number'){
+        result = input1 + input2;
+    }else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-//person.role.push('Admin');//A person can have only one role but TypeScript won't know that
-//person.role[1] = 10;//role array of string and/or number but TypeScript won't know that second field is String
-//To overcome this TypeScript have added another type which Tuple 
-//person.role = [5,'Admin','User'];//Its giving error as role is defined to have only two parameteres.
+const combinedAges = combine(30,26);
+console.log(combinedAges);
 
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-}
+const combinedNames = combine('Vijay','Kumar');
+console.log(combinedNames);
---
+++ core-types.ts
@@ -0,0 +1,39 @@
+/* const person: {
+    name: string;
+    age: number;
+} = { */
+/* const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number,string];//To define Tuple type we need to define the type of object
+} = {
+    name: "Vijay",
+    age: 37,
+    hobbies: ['Sports','Cooking'],
+    role: [2,'Author']
+} */
+
+//person.role.push('Admin');//A person can have only one role but TypeScript won't know that
+//person.role[1] = 10;//role array of string and/or number but TypeScript won't know that second field is String
+//To overcome this TypeScript have added another type which Tuple 
+//person.role = [5,'Admin','User'];//Its giving error as role is defined to have only two parameteres.
+
+enum Role {ADMIN,READ_ONLY,AUTHOR};
+//enum Role {ADMIN = 5 ,READ_ONLY = "READ_ONLY",AUTHOR = 100};//This is also valid
+
+const person = {
+    name: "Vijay",
+    age: 37,
+    hobbies: ['Sports','Cooking'],
+    role: Role.ADMIN
+}
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
