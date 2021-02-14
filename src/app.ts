b063a8c7140df2a17a3ce458ee222bbba8ed5cf6
vijaykglg <vijay.gupta7@yahoo.in>
2021-02-14 21:33:55 +0530

    interfaces in TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,106 +1,31 @@
-class Department{
-    //we can use constructor to define the class fields directly
-    /* private id: string;
-    private name: string; */
-    protected employees: string[] = [];
 
-    constructor(private readonly id: string,public name: string){
-        //this.name = s;
+interface Named{
+    readonly name: string;
+}
+
+interface Greetable extends Named{
+    greet(phrase: string): void;
+}
+
+class Person implements Greetable{
+    name: string;
+    age = 30;
+    constructor(name: string){
+        this.name = name;
     }
 
-    describe(this: Department){//To have extra layer of Type Safty we can add this refer line 20
-        //console.log('Department: '+this.name);
-        console.log(`Department - (${this.id}) : (${this.name})`);//other way of writing
-    }
-
-    addEmployee(employee: string){
-        this.employees.push(employee);
-    }
-
-    printEmployees(){
-        console.log(this.employees.length);
-        console.log(this.employees);
+    greet(phrase: string){
+        console.log(phrase +' '+this.name);
     }
 }
 
-class ITDepartment extends Department{
-    admins: string[];//this class field is specific to ITDepartment class
-    constructor(id: string,admins: string[]){
-        super(id,'IT');
-        this.admins = admins;
-    }
-}
+let user1: Greetable;
+user1 = new Person('VIJAY GUPTA');
+user1.greet('Hi There - I am');
 
-const accounting = new Department('d1','Accounting');
-accounting.describe();
-accounting.addEmployee('Vijay');
-accounting.addEmployee('Kumar');
+let user2: Greetable;
+user2 = new Person('KUMAR');
+user2.greet('Hi There - I am');
 
-//accounting.employees[2] = 'Gupta';//If not private then we can access the filed out side of the class
-
-accounting.printEmployees();
-
-const itDepartment = new ITDepartment('d1',['Max']);
-itDepartment.describe();
-itDepartment.addEmployee('Max');
-itDepartment.addEmployee('Milan');
-
-//accounting.employees[2] = 'Gupta';//If not private then we can access the filed out side of the class
-
-itDepartment.printEmployees();
-console.log(itDepartment);
-
-class AccountingDepartment extends Department{
-    private lastReport: string;
-
-    get getLastReport(){
-        if(this.lastReport){
-            return this.lastReport;
-        }
-        throw new Error('No Report Found!!');
-    }
-
-    set setLastReport(value: string){
-        if(!value){
-            throw new Error('Please enter a valid value');
-        }
-        this.addReports(value);
-    }
-
-    constructor(id: string,public reports: string[]){
-        super(id,'Accounting');
-        this.lastReport = reports[0];
-    }
-
-    addEmployee(employee: string){
-        if(employee !== 'Max'){
-            this.employees.push(employee);    
-        }
-    }
-
-    addReports(text: string){
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-
-    printReports(){
-        console.log(this.reports);
-    }
-}
-
-const accountingDept = new AccountingDepartment('d2',[]);
-accountingDept.addEmployee('Trump');
-accountingDept.addEmployee('Milenia');
-accountingDept.addEmployee('Max');//This will not be added to the employee array
-
-accountingDept.addReports('Something went wrong...');
-accountingDept.getLastReport;
-accountingDept.setLastReport = 'Year End Report';
-accountingDept.printReports();
-console.log(accountingDept);
-
-/* 'this' refers to the calling object.
-If you assign your class method to another copy object then 'this' will not be able to read the fields which is not available in your copied object. */
-
-/* const accountingCopy = {name: 'DUMMY',describe: accounting.describe};//Added name property to match the Department class
-accountingCopy.describe();//This will give undefined */
+console.log(user1 === user2);//gives false
+console.log(user1 instanceof Person);
---
+++ src/classes.ts
@@ -0,0 +1,130 @@
+abstract class Department{
+    //we can use constructor to define the class fields directly
+    /* private id: string;
+    private name: string; */
+    protected employees: string[] = [];
+
+    constructor(protected readonly id: string,public name: string){
+        //this.name = s;
+    }
+
+    /* describe(this: Department){//To have extra layer of Type Safty we can add this refer line 20
+        //console.log('Department: '+this.name);
+        console.log(`Department - (${this.id}) : (${this.name})`);//other way of writing
+    } */
+
+    abstract describe(this: Department): void;//Abstract method
+
+    addEmployee(employee: string){
+        this.employees.push(employee);
+    }
+
+    printEmployees(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+class ITDepartment extends Department{
+    admins: string[];//this class field is specific to ITDepartment class
+    constructor(id: string,admins: string[]){
+        super(id,'IT');
+        this.admins = admins;
+    }
+
+    //Implementing the abstract mthod below
+    describe(){
+        console.log('IT Department - ID = '+ this.id);
+    }
+}
+
+/* const accounting = new Department('d1','Accounting');
+accounting.describe();
+accounting.addEmployee('Vijay');
+accounting.addEmployee('Kumar');
+
+//accounting.employees[2] = 'Gupta';//If not private then we can access the filed out side of the class
+
+accounting.printEmployees(); */
+
+const itDepartment = new ITDepartment('d1',['Max']);
+itDepartment.describe();
+itDepartment.addEmployee('Max');
+itDepartment.addEmployee('Milan');
+
+//accounting.employees[2] = 'Gupta';//If not private then we can access the filed out side of the class
+
+itDepartment.printEmployees();
+console.log(itDepartment);
+
+class AccountingDepartment extends Department{
+    private lastReport: string;
+    private static instance: AccountingDepartment;
+
+    get getLastReport(){
+        if(this.lastReport){
+            return this.lastReport;
+        }
+        throw new Error('No Report Found!!');
+    }
+
+    set setLastReport(value: string){
+        if(!value){
+            throw new Error('Please enter a valid value');
+        }
+        this.addReports(value);
+    }
+
+    private constructor(id: string,public reports: string[]){
+        super(id,'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    static getInstance(){
+        if(AccountingDepartment.instance){
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2',[]);
+        return this.instance;
+    }
+
+    describe(){
+        console.log('Accounting Department - ID = '+this.id);
+    }
+
+    addEmployee(employee: string){
+        if(employee !== 'Max'){
+            this.employees.push(employee);    
+        }
+    }
+
+    addReports(text: string){
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+
+    printReports(){
+        console.log(this.reports);
+    }
+}
+
+//const accountingDept = new AccountingDepartment('d2',[]);
+const accountingDept = AccountingDepartment.getInstance();
+const accountingDept1 = AccountingDepartment.getInstance();
+console.log(accountingDept,accountingDept1);
+accountingDept.addEmployee('Trump');
+accountingDept.addEmployee('Milenia');
+accountingDept.addEmployee('Max');//This will not be added to the employee array
+
+accountingDept.addReports('Something went wrong...');
+accountingDept.getLastReport;
+accountingDept.setLastReport = 'Year End Report';
+accountingDept.printReports();
+accountingDept.describe();
+console.log(accountingDept);
+
+/* 'this' refers to the calling object.
+If you assign your class method to another copy object then 'this' will not be able to read the fields which is not available in your copied object. */
+
+/* const accountingCopy = {name: 'DUMMY',describe: accounting.describe};//Added name property to match the Department class
+accountingCopy.describe();//This will give undefined */
